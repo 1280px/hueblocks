375b9e0add96f3eba3b76fda6d7cb7b3dd63508d
1280px <71165491+1280px@users.noreply.github.com>
2025-01-02 22:40:57 +0700

    Add j2bs name & color calculation

--- JAR TO BLOCKSET/jar2blockset.py
+++ JAR TO BLOCKSET/jar2blockset.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from shutil import rmtree
-import datetime
 
 import numpy as np
 from PIL import Image
@@ -70,11 +69,9 @@ def get_blacklisted_blocks(blacklist_path, blocks_path):
     blocks_names = sorted(os.listdir(blocks_path))
 
     with open(blacklist_path, 'r') as blacklist_f:
-        # blacklist = load(blacklist_f, Loader=Loader)
         blacklist_raw = safe_load(blacklist_f)
 
     blacklist = compute_filter(blacklist_raw['filters'], blocks_names)
-    test = blacklist.keys()
 
     # We don't care about metadata in this case, so we just strip it out
     return list(blacklist.keys())
@@ -85,7 +82,6 @@ def get_autoparse_facing(autoparse_path, blocks_path):
     blocks_names = sorted(os.listdir(blocks_path))
 
     with open(autoparse_path, 'r') as autoparse_f:
-        # blacklist = load(autoparse_f, Loader=Loader)
         autoparse_raw = safe_load(autoparse_f)
 
     # First we compute filters for every single facing option (bruh!!1)
@@ -93,7 +89,7 @@ def get_autoparse_facing(autoparse_path, blocks_path):
         side: {} for side in sides
     }
     for side in facing_filters.keys():
-        print(side)
+        # print(side)
         facing_filters[side] = compute_filter(autoparse_raw['facing'][side], blocks_names)
 
     # Now we need to convert this object of objects into a list of pairs
@@ -106,7 +102,7 @@ def get_autoparse_facing(autoparse_path, blocks_path):
         name_present = {
             side: facing_filters[side].get(name, 'X') for side in sides
         }
-        print(name, name_present)
+        # print(name, name_present)
 
         present_sides = []
         # Since "all" option currently includes overrides only, it has the
@@ -131,7 +127,7 @@ def get_autoparse_facing(autoparse_path, blocks_path):
             # No filters matched, meaning this texture is used on all sides
             else:
                 present_sides = sides.copy()[:-2]
-        print(present_sides, '\n')
+        # print(present_sides, '\n')
 
         textures_to_sides[name] = present_sides
     return textures_to_sides
@@ -139,15 +135,107 @@ def get_autoparse_facing(autoparse_path, blocks_path):
 
 def get_autoparse_naming(autoparse_path):
     with open(autoparse_path, 'r') as autoparse_f:
-        # blacklist = load(autoparse_f, Loader=Loader)
         autoparse_raw = safe_load(autoparse_f)
     
     naming_rules = autoparse_raw['naming']
     return naming_rules
 
 
-def generate_textures_data(autoparse_facing, autoparse_naming, blocks_path):
-    raise NotImplementedError
+def get_autoparse_colorcalc(autoparse_path):
+    with open(autoparse_path, 'r') as autoparse_f:
+        autoparse_raw = safe_load(autoparse_f)
+    
+    colorcalc_rule = autoparse_raw['colorcalc']
+    return colorcalc_rule
+
+
+def generate_naming(string, naming_rule):
+    if naming_rule == 'none':
+        return string
+
+    string_new = os.path.splitext(string)[0]
+    # Underscores used by block assets, and dashes commonly used in JAR filenames
+    string_new = ' '.join(string_new.split('_'))
+    string_new = ' '.join(string_new.split('-'))
+
+    if naming_rule == 'title':
+        return string_new.title()
+    elif naming_rule == 'capital':
+        return string_new.capitalize()
+    elif naming_rule == 'sep':
+        return string_new
+
+    else:
+        print(f'Incorrect naming rule "{naming_rule}"!')
+        sys.exit(1)
+    
+
+def calc_avg_color(texture_path, colorcalc_rule):
+    img = Image.open(texture_path)
+
+    # Modern method -- instead of calculating avg color as literal averages
+    # of each RGB channel (+- resampling artefacts), find sqrts of mean of
+    # each channel squared. This results in result color being more vibrant,
+    # closer to original image brightness! Check this amazing article and
+    # video in it: https://sighack.com/post/averaging-rgb-colors-the-right-way
+    if colorcalc_rule == 'modern':
+        img_np = np.asarray(
+            img.convert("RGB"),
+            dtype=np.float64 # Otherwise will almost certainly overflow 
+        )
+        img_np = img_np ** 2
+
+        mean_of_squares = np.mean(img_np, axis=(0, 1))
+        sqrts_of_mean_of_squares = np.floor(
+            np.sqrt(mean_of_squares),
+            # dtype=np.uint8
+        ).astype(np.uint8) # Prevent color being out of 0-255 range
+
+        # Compare results!
+        img_proc = img.resize((1, 1), Image.Resampling.LANCZOS)
+        img_proc_np = np.asarray(img_proc.convert("RGB"))
+        img_color = img_proc_np[0][0]
+        print(texture_path, '\nlegacy:', list(img_color), 'modern:', list(sqrts_of_mean_of_squares), '\n')
+
+        return list(sqrts_of_mean_of_squares)
+
+    # Legacy method -- just like it worked in older versions of HueBlocks:
+    # using PIL antialias, shrink image to a single pixel and take its color
+    elif colorcalc_rule == 'legacy':
+        # Image.LANCZOS does exactly the same as now-deprecated Image.ANTIALIAS
+        img_proc = img.resize((1, 1), Image.Resampling.LANCZOS)
+        img_proc_np = np.asarray(img_proc.convert("RGB"))
+        img_color = img_proc_np[0][0]
+
+        return list(img_color)
+
+    else:
+        print(f'Incorrect colorcalc rule "{colorcalc_rule}"!')
+        sys.exit(1)
+
+
+
+def generate_textures_data(textures_path, facing_filters, naming_rule, colorcalc_rule):
+    textures_data = []
+
+    for texture_name in sorted(os.listdir(textures_path)):
+        texture_sides = facing_filters.get(
+            texture_name,
+            # Fallback just in case (should never be used actually)
+            ['top', 'bottom', 'north', 'south', 'east', 'west']
+        )
+        texture_path = os.path.join(textures_path, texture_name)
+
+        texture_data = {
+            'name': generate_naming(texture_name, naming_rule),
+            # Full path can later be computed using blockset's dir property
+            'texture': texture_name,
+            'color': calc_avg_color(texture_path, colorcalc_rule),
+            'sides': texture_sides
+        }
+        textures_data.append(texture_data)
+
+    return textures_data
 
 
 if __name__ == "__main__":
@@ -168,11 +256,16 @@ if __name__ == "__main__":
     # print(JAR_PATH, EXCLUDE_ALPHA, EXCLUDE_ANIMATED, BLACKLIST_PATH, AUTOPARSE_PATH)
     OUTPUT_PATH = os.path.splitext(JAR_PATH)[0]
 
-    # Ask what to do if output directory already exists
+
+    # Ask what to do if generated blockset files already exist
     if os.path.isdir(OUTPUT_PATH):
         if input(f'\nOutput dir "{OUTPUT_PATH}" already exists! Type Y to replace: ').upper() == 'Y':
             rmtree(OUTPUT_PATH)
     os.mkdir(OUTPUT_PATH)
+
+    if os.path.isfile(OUTPUT_PATH + '.json'):
+        if input(f'\nBlockset data "{OUTPUT_PATH + ".json"}" already exists! Type Y to delete: ').upper() == 'Y':
+            os.remove(OUTPUT_PATH + '.json')
 
 
     # Extract block texture assets from jar
@@ -249,18 +342,23 @@ if __name__ == "__main__":
     print(f'Filtered out down to {len(os.listdir(OUTPUT_PATH))} assets\n')
 
 
-    # (D) TODO: Generate blockset data about each texture
+    # (D) Generate blockset data about each texture
     autoparse_facing_filters = get_autoparse_facing(AUTOPARSE_PATH, OUTPUT_PATH)
     autoparse_naming_rules = get_autoparse_naming(AUTOPARSE_PATH)
-    print(autoparse_facing_filters, autoparse_naming_rules)
+    autoparse_colorcalc_rule = get_autoparse_colorcalc(AUTOPARSE_PATH)
+    print('Computed autoparse rules, processing textures...\n')
 
     blockset_data = {
-        'name': None,
-        'dir': None,
-        'count': None,
-        'data': generate_textures_data(os.path.join(OUTPUT_PATH), autoparse_facing_filters, autoparse_naming_rules)
+        'name': generate_naming(os.path.split(JAR_PATH)[-1], autoparse_naming_rules['blockset']),
+        'dir': OUTPUT_PATH,
+        'count': len(os.listdir(OUTPUT_PATH)),
+        'data': generate_textures_data(
+            OUTPUT_PATH,
+            autoparse_facing_filters, autoparse_naming_rules['textures'], autoparse_colorcalc_rule
+        )
     }
+    print('Blockset generation finished!\n')
 
-
-    # TODO: Finally, fill out remaining blockset data and save as JSON file
+    # Finally, save blockset data as JSON
     raise NotImplementedError
+    print('Blockset data saved as {OUTPUT_PATH + ".json"}')
