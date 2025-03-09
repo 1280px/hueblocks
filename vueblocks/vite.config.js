0035b751a260c2d4321870ad5b09084d950b4dc3
twelve eighty pix. <71165491+1280px@users.noreply.github.com>
2025-03-09 23:11:49 +0700

    Update vite.config.js

--- vueblocks/vite.config.js
+++ vueblocks/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: "/hueblocks-new",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
