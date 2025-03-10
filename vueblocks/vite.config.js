82833b2bf10af56c6486023a4921c67abafa46c2
twelve eighty pix. <71165491+1280px@users.noreply.github.com>
2025-03-10 21:34:46 +0700

    Revert vite.config.js base change

--- vueblocks/vite.config.js
+++ vueblocks/vite.config.js
@@ -4,14 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  base: "/vueblocks",
+  base: "/hueblocks-new",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
